6a534d628956f76c9dc4476c254ea6c61b438e99
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-12-15 10:06:39 +0100

    feat: Use get_pdf() function

---
+++ analyses/05-download-pdf.R
@@ -0,0 +1,42 @@
+#' Agri-TE project
+#' 
+#' Download PDF using the famous website Sc.-H.b.
+#' 
+#' @author Nicolas Casajus \email{nicolas.casajus@fondationbiodiversite.fr}
+#' 
+#' @date 2022/12/15
+
+
+
+## Open unique references ----
+
+unique_refs <- readxl::read_xlsx(here::here("outputs", 
+                                            "unique_primary_studies_with_doi.xlsx"))
+
+
+## Create PDFs directory ----
+
+path_pdf <- here::here("outputs", "pdfs")
+
+dir.create(path_pdf, showWarnings = FALSE, recursive = TRUE)
+
+
+for (i in 1:nrow(unique_refs)) {
+
+  ## Get DOI ----
+  
+  doi <- c(unique_refs[i, "DOI"], unique_refs[i, "best_doi"])
+  
+  is_dois <- unlist(lapply(dois, is_valid_doi))
+  dois <- dois[which(is_dois)]
+  
+  
+  ## Download PDF if possible ----
+  
+  if (length(doi) == 1) {
+    
+    get_pdf(doi, path = path_pdf, filename = unique_refs[i, "noid"])
+    
+    Sys.sleep(sample(seq(0, 5, by = 0.01), 1))
+  }
+}
